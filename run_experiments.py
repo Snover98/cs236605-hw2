e5589d5784b7f65ad676cb7e7e0b202bb87edb42
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-05-10 15:27:05 +0200

    made pooling maybe make more sense

--- run_experiments.py
+++ run_experiments.py
@@ -14,7 +14,7 @@ def exp1_1():
         for L in [2, 4, 8, 16]:
             exp_name = f'exp1_1_K{K}_L{L}'
             print(f"CURRENT RUN: {exp_name}")
-            run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L, pool_every=2,
+            run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L, pool_every=L,
                            hidden_dims=[1024])
 
 
@@ -23,7 +23,7 @@ def exp1_2():
     for L in [2, 4, 8]:
         for K in [32, 64, 128, 258]:
             exp_name = f'exp1_2_L{L}_K{K}'
-            run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L, pool_every=2,
+            run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L, pool_every=L,
                            hidden_dims=[1024])
 
 
@@ -32,7 +32,7 @@ def exp1_3():
     K = [64, 128, 256]
     for L in [1, 2, 3, 4]:
         exp_name = f'exp1_3_L{L}_K{K[0]}-{K[1]}-{K[2]}'
-        run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L, pool_every=2, hidden_dims=[1024])
+        run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L, pool_every=L, hidden_dims=[1024])
 
 
 def exp2():
