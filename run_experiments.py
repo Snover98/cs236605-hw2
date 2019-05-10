fe8c970de0aff6e8939e84de23b3896882f9ee73
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-05-10 14:23:38 +0200

    added file for running all experiments, currently does not run exp2

---
+++ run_experiments.py
@@ -0,0 +1,44 @@
+from hw2.experiments import run_experiment
+
+
+def main():
+    exp1_1()
+    exp1_2()
+    exp1_3()
+    # exp2()
+
+
+def exp1_1():
+    print("Running experiment 1.1")
+    for K in [32, 64]:
+        for L in [2, 4, 8, 16]:
+            exp_name = f'exp1_1_K{K}_L{L}'
+            run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L)
+
+
+def exp1_2():
+    print("Running experiment 1.2")
+    for L in [2, 4, 8]:
+        for K in [32, 64, 128, 258]:
+            exp_name = f'exp1_2_L{L}_K{K}'
+            run_experiment(run_name=exp_name, filters_per_layer=[K], layers_per_block=L)
+
+
+def exp1_3():
+    print("Running experiment 1.3")
+    K = [64, 128, 256]
+    for L in [1, 2, 3, 4]:
+        exp_name = f'exp1_3_L{L}_K{K[0]}-{K[1]}-{K[2]}'
+        run_experiment(run_name=exp_name, filters_per_layer=K, layers_per_block=L)
+
+
+def exp2():
+    print("Running experiment 2")
+    K = [64, 128, 256, 512]
+    for L in [1, 2, 3, 4]:
+        exp_name = f'exp2_L{L}_K{K[0]}-{K[1]}-{K[2]}'
+        run_experiment(run_name=exp_name, filters_per_layer=K, layers_per_block=L, ycn=True)
+
+
+if __name__ == '__main__':
+    main()
